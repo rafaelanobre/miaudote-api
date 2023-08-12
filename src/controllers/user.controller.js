5593362ff51702bd9c4090d476637b5297b3db5c
rafaelanobre <raafanobre@gmail.com>
2023-08-11 23:42:48 -0300

    :sparkles: Feat: get user pets and info

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,38 @@
+import { db } from "../database/database.connection.js";
+
+export async function getUserInfo(req,res){
+    const userId = req.userId;
+    try{
+        const { rows: [userInfo] } = await db.query(`
+            SELECT
+                u.name AS user_name,
+                u.email,
+                u.cpf,
+                u.cellphone,
+                json_agg(
+                    json_build_object(
+                        'registeredAt', p."registeredAt",
+                        'petName', p.name,
+                        'categoryId', p."categoryId",
+                        'photo', p.photo,
+                        'available', p.available,
+                        'city', p.city,
+                        'state', p.state
+                    )
+                ) AS pets
+            FROM users AS u
+            LEFT JOIN pets AS p ON u.id = p."ownerId"
+            WHERE u.id = $1
+            GROUP BY u.id
+        `, [userId]);
+
+        if (!userInfo) {
+            return res.status(404).send({ message: 'Usuário não encontrado.' });
+        }
+
+        res.status(200).send(userInfo);
+    }catch(err){
+        const errorMessage = err ? err : "Ocorreu um erro interno no servidor.";
+        res.status(500).send(errorMessage);
+    }
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import validateToken from "../middlewares/validateToken.js";
+import { getUserInfo } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/my-account", );
+userRouter.get("/my-account", validateToken, getUserInfo);
 userRouter.put("/update-profile", );
 userRouter.put("/change-password", );
 
